840a1d64880aea85d010ece60a0a40d2bbaa300f
andre <andrefrateschi@gmail.com>
2020-09-02 19:10:30 -0300

    commit 01

---
+++ arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace arrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] linguagens = new string[4];
+            linguagens[0] = "c#";
+            linguagens[1] = "JS";
+            linguagens[2] = "java";
+            linguagens[3] = "cobol";
+
+            Console.WriteLine("linguagem de programacao: 0 " + linguagens[0]);
+            Console.WriteLine("linguagem de programacao: 1 " + linguagens[1]);
+            Console.WriteLine("linguagem de programacao: 2 " + linguagens[2]);
+            Console.WriteLine("linguagem de programacao: 3 " + linguagens[3]);
+
+
+        }
+    }
+}
